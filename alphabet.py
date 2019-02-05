d71a88108d07bfe5f919f4831b0d4daf2e417aa8
sandhiya4570 <47344041+sandhiya4570@users.noreply.github.com>
2019-02-05 11:47:11 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+choice = input("Enter a value: ")
+if((choice>='a' and choice<= 'z') or (choice>='A' and choice<='Z')):
+    print(choice, "is an Alphabet")
+else:
+    print(choice, "is not an Alphabet")
