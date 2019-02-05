91a78f490c832cc8bb4c5cda75ba56c87aa7db05
sandhiya4570 <47344041+sandhiya4570@users.noreply.github.com>
2019-02-05 11:21:01 +0530

    Update positivenegative.py

--- positivenegative.py
+++ positivenegative.py
@@ -1,8 +1,9 @@
 num = float(input(|"enter a number:"))
 if num>=0:
- if num ==0;
-  print("zero")
-  else:
-  print("positive number")
-  else:
-  print(negative number")
+  if num ==0:
+    if num ==0:
+       print("zero")
+    else:
+       print("positive number")
+    else:
+       print(negative number")
