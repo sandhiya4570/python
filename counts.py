e2a82dec401ccee54d0a1a2af6d25c008ab2d310
sandhiya4570 <47344041+sandhiya4570@users.noreply.github.com>
2019-08-14 16:08:28 +0530

    Update counts.py

--- counts.py
+++ counts.py
@@ -1,11 +1,2 @@
-string=input("Enter string:")
-char=0
-word=1
-for i in string:
-      char=char+1
-      if(i==' '):
-            word=word+1
-print(" ")
-print(word)
-print(" ")
-print(char
+a = "laptop is good"
+print(len(a) = a.count(" "))
