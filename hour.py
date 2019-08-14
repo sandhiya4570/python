6c22257ff55c68347ed9a3a76b903f012471db72
sandhiya4570 <47344041+sandhiya4570@users.noreply.github.com>
2019-08-14 15:36:31 +0530

    Update hour.py

--- hour.py
+++ hour.py
@@ -1,9 +1,9 @@
 time = float(input("Input time in seconds: "))
 day = time // (24 * 3600)
 time = time % (24 * 3600)
-hour = time // 3600
+hour = time 
 time %= 3600
-minutes = time // 60
+minutes = time
 time %= 60
 seconds = time
 print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
